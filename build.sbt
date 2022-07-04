c7b23e65330c19343a5de234e842a381b6f80c6d
Bilal <nasim.bigo@gmail.com>
2022-07-04 17:25:54 +0200

    Some changes

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "MoviesSimilarity"
+
+version := "0.1"
+
+scalaVersion := "2.12.14"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.0.0",
+  "org.apache.spark" %% "spark-sql" % "3.0.0"
+)
---
+++ src/main/scala/com/sparksbt/sprak/MoviesR_Bail.scala
@@ -0,0 +1,66 @@
+package com.sparksbt.sprak
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.log4j._
+
+object MoviesR_Bail {
+
+  case class movies(userID : Int, movieID : Int, rating : Int, timestamp: Long)
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = new SparkSession.Builder().appName("Movies").master("local").getOrCreate()
+
+    import spark.implicits._
+    val movieSchema = new StructType()
+      .add("userID", IntegerType)
+      .add("movieID", IntegerType)
+      .add("rating", IntegerType)
+      .add("timestamp", LongType)
+
+    val movieSD = spark.read.option("sep","\t")
+      .schema(movieSchema)
+      .csv("C:/DEV/SparkScalaCourse/data/ml-100k/u.data").as[movies]
+    movieSD.cache()
+
+    val User2Movies = movieSD.as("movieSD1")
+      .join(movieSD.as("movieSD2"),
+        col("movieSD1.userID") === col("movieSD1.userID"),
+        "inner")
+      .filter(col("movieSD1.movieID")=!= col("movieSD2.movieID"))
+      .select(col("movieSD1.userID").as("userID"),
+        (when(col("movieSD1.movieID")>col("movieSD2.movieID"),
+          col("movieSD2.movieID")).otherwise(col("movieSD1.movieID"))).as("movieID1"),
+        (when(col("movieSD1.movieID")>col("movieSD2.movieID"),
+          col("movieSD2.rating")).otherwise(col("movieSD1.rating"))).as("rating1"),
+        (when(col("movieSD1.movieID")>col("movieSD2.movieID"),
+          col("movieSD1.movieID")).otherwise(col("movieSD2.movieID"))).as("movieID2"),
+        (when(col("movieSD1.movieID")>col("movieSD2.movieID"),
+          col("movieSD1.rating")).otherwise(col("movieSD2.rating"))).as("rating2"))
+      .distinct()
+
+    val MoviesSim = User2Movies.groupBy("movieId1","movieId2")
+      .agg(sum((col("rating1") + col("rating2"))/2).as("Similarity")
+        ,count("*").as("RatingNumber"))
+
+    val MoviesSimCal = MoviesSim.select(col("movieId1"),col("movieId2")
+      ,(round(col("Similarity")/col("RatingNumber"),0))
+        .cast("int").as("SimilarityRating")
+      ,col("RatingNumber"))
+    val Similarity = MoviesSimCal.orderBy(desc("RatingNumber"), desc("SimilarityRating"))
+
+    print(Similarity.show(30))
+    //result.show(10)
+
+
+
+
+    spark.stop()
+
+  }
+
+
+
+
+}
